526bd69b24ceaddf8989c4bca7efdc00f8bf416c
Apollinariafil <filonchikp@yandex.ru>
2025-01-16 00:41:44 +0300

    1

---
+++ dz1501_14.py
@@ -0,0 +1,25 @@
+#for x in range(2030, 1, -1):
+ #   x1 = 7**91 + 7**160 - x
+  #  cnt = 0
+   # while x1 != 0:
+    #       cnt += 1
+        # x1 //= 7
+     #   x1 = x1 // 7
+    #if cnt == 70:
+     #   print(x)
+      #  break
+# s = "2" * 400
+# while("8888") in s or "222" in s:
+#     if "222" in s:
+#         s = s.replace("222", "88", 1)
+#     else:
+#         s = s.replace("8888", "22", 1)
+F = [1] * 5000
+for n in range(len(F)):
+    if n > 3456:
+        F[n] = n + 1
+    if n <= 3456 and n % 3 == 0:
+        F[n] = F[n + 1] + F[n + 2]
+    if n <= 3456 and not(n % 3 == 0):
+        F[n] = F[n + (n % 3)] + 2
+print(F[12]- F[17])
