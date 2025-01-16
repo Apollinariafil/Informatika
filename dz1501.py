0fd8096ccaee6eae19c1680d8112ed17263b46ab
Aleksander <fil.ap@bk.ru>
2025-01-16 13:52:13 +0300

    Готовы результат

--- dz1501.py
+++ dz1501.py
@@ -1,6 +1,10 @@
+# from functools import lru_cache
+# @lru_cache(None)
+
 import sys
 # Увеличение глубины рекурсии до 3000
 sys.setrecursionlimit(3000)
+
 
 def F(n):
     if n > 3456:
@@ -12,3 +16,26 @@ def F(n):
 
 
 print(F(12)-F(17))
+
+
+# F = [1] * 5000
+# for n in range(len(F)):
+#     # if n <= 1:
+#     #     F[n] = 1
+#     if n > 3456 and (n > 1):
+#         F[n] = n + 1
+#     if n <= 3456 and ((n % 3) == 0) and (n > 1):
+#         F[n] = F[n - 1] + F[n - 2]
+#     if n <= 3456 and ((n % 3) != 0) and (n > 1):
+#         F[n] = F[n - (n % 3)] + 2
+# print(F[12])
+
+
+# for n in range(len(F)):
+#     if n >= 2025:
+#         F[n] = n
+#     if n < 2025:
+#         F[n] = n + 3 + F[n + 3]
+# print(F[23])
+
+
